b748dd4886f9e5014fe66e8fc1666d937adef3d1
李宪伟 <leexw1994@gmail.com>
2017-06-14 23:58:13 +0800

    test

---
+++ WebCrawl/test.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+"""
+@version: ??
+@author: 李宪伟
+@license: Apache Licence 
+@contact: lixw1994@yeah.net
+@site: 
+@software: PyCharm
+@file: test.py
+@time: 2016/12/3 下午7:14
+"""
+
+if __name__ == '__main__':
+    pass
